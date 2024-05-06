c30d5babcf692fc7755dddbbcd22557f566ec791
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2024-05-06 14:05:22 +0700

    v0.3.0
    In borris v0.3.0:
    1. Ynew = (Yo + (Vy * Sx) / Vx) % 32
    2. takes into consideration Vy < 0
    3. gives out the correct Ynew (borris 0.2.0 didn't always do that)

--- borris.asm
+++ borris.asm
@@ -1,19 +1,28 @@
-# this is borris:
-# Y = (Yo + Vy * (Sx / Vx)) % 32
+# this is borris v0.3.0:
+# Y = (Yo + (Vy * Sx) / Vx) % 32
 
-# (Sx / Vx) = t
+# Vy = (Vy * Sx)
 # <=>
-# while Sx > Vx:
-#		Sx -= Vx
+# t = Sx
+# while t > 0:
+#		Vy += Vy
+#		t --
+
+
+
+# (Vy / Vx) = t
+# <=>
+# t = 0
+# while Vy > Vx:
+#		Vy -= Vx
 #		t ++
 
-# Vy * t
-# Yo + Vy * t
-# <=>
-# for i in range(t):
-# 		Yo += Vy
+# Yo = Yo (+/-) t
 
-# Yo %= 32 (cut of the top 3 bits essentially) —— example: 64 = 01000000
+#NOT REALLY # Yo %= 32 (cut of the top 3 bits essentially) —— example: 64 = 01000000
+
+# Vy: 0xe0 == 0 => going up		(Vy >= 0)
+#	  0xe0 == 1 => going down	(Vy < 0)
 
 asect 0xf0
 Sx:
@@ -64,53 +73,136 @@ borris:
 		ld r0,r1	# Load r1 from data memory 
        tst r1		# Test if r1 is 0
 	until gt
+
+	# Vy: 0xe0 == 0 => going up		(Vy >= 0)
+	#	   0xe0 == 1 => going down (Vy < 0)
+	ldi r0, 0xe0
+	if
+		tst r1
+	is	mi
+		neg r1
+		ldi r2, 1
+	else
+		ldi r2, 0
+	fi
+	st r0, r2
 	
 	ldi r0, 0x03	# Load the address for Vy
 	st r0,r1		# Store Vy at 0x03
 	
 
-	ldi r2, 0		# t
+	# Begin: Vy = (Vy * Sx)
+	ldi r2, 0x00		# t = Sx
 	
 	ldi r0, 0x00	# Sx
 	ld r0, r0
 	
-	ldi r1, 0x02	# Vx
+	ldi r1, 0x03	# Vy
 	ld r1, r1
-	
-	# (Sx / Vx) = t
+
+	# Vy = (Vy * Sx) 
 	while
-		cmp r0, r1
-	stays gt
-		neg r1
-		add r1, r0
-		neg r1
-		inc r2
+		tst r2		# while t > 0:
+	stays pl
+		add r1, r1	# Vy += Vy
+		neg r2		# t --
 	wend
-	inc r2
+	# neg r2
+	# End: Vy = (Vy * Sx)
+
+	# Begin: t = (Vy / Vx)
+	# Vy is loaded in r1
 	
-	ldi r0, 0x01	# Sy
+	ldi r0, 0x02  # Vx
 	ld r0, r0
 	
-	ldi r1, 0x03  # Vy
+	ldi r3, 0		# t
+	
+	# t = (Vy / Vx)
+	while
+		cmp r1, r0	# while Vy > Vx:
+	stays	nz
+		inc r3		# t++
+		neg r0		# Vy -= Vx
+		add r0, r1
+		neg r0
+	wend
+	inc r3
+	# End: t = (Vy / Vx)
+	
+	
+	# t is loaded into r3
+	# Begin: Yo = Yo (+/-) t
+	ldi r0, 0x01	# Sy (<=> Yo)
+	ld r0, r0
+	
+	ldi r1, 0xe0	# sign of Vy
 	ld r1, r1
 	
+	if
+		tst r1
+	is nz
+		neg r3
+	fi
+	add r3, r0
+	# End: Yo = Yo (+/-) t
 	
-	# Yo = Yo + Vy * t
-	while
-		dec r2
-	stays	nz
-		add r1, r0
-	wend
-	add r1, r0
+	
+	# sign flag for Yo (0 — >0; 1 — <0)
+	if
+		tst r0
+	is	mi
+		neg r0
+		ldi r1, 1
+	else
+		ldi r1, 0
+	fi
+	ld r1, r1
 	
 	
 	# r0 %= 32
-	shl r0
-	shl r0
-	shl r0
+	shla r0
+	shla r0
+	shla r0
+	shla r0
+	# проверка на "6й бит" (спросить у данила)
+	ldi r2, 0xe1
+	if
+		tst r0
+	is cs
+		ldi r2, 1
+	else
+		ldi r2, 0
+	fi
+	ld r2, r2
+	
 	shr r0
 	shr r0
 	shr r0
+	shr r0
+
+
+	if
+		tst r1
+	is z
+		if
+			tst r2
+		is nz
+			# return 32 - (r0 % 32)
+			ldi r3, -32
+			add r3, r0
+			neg r0
+		fi
+	else
+		if
+			tst r2
+		is z
+			# return 32 - (r0 % 32)
+			ldi r3, -32
+			add r3, r0
+			neg r0
+		fi
+	fi
 	
 	ldi r1, 0xf4	# New Y
 	st r1, r0

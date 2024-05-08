729cbdb282abca18d809e1bae76ea60c5e92daaa
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2024-05-08 23:49:12 +0700

    v0.3.1
    @LessVegetables
    1. Bot change: different way of calculating  Ynew %=32;
    2. Still has a bug where it doesn't quite properly calculate the ball position if it hits the wall an odd number of times.

--- borris.asm
+++ borris.asm
@@ -38,41 +38,54 @@ Y:
 asect 0x04
 
 borris:
+	ldi r0, SX
+	ld r0, r0
+	ldi r1, Sx
+	st r1, r0
 	ldi r0, Sx		# Load the address of SxIO
 	do      		# Begin the keyboard read loop
 		ld r0,r1	# Load r1 from data memory 
        tst r1		# Test if r1 is 0
-	until gt
+	until nz
 	
 	ldi r0, 0x00	# Load the address for Sx
 	st r0,r1		# Store Sx at 0x00
 	
-	
+	ldi r0, SY
+	ld r0, r0
+	ldi r1, Sy
+	st r1, r0
 	ldi r0, Sy		# Load the address of SyIO
 	do      		# Begin the keyboard read loop
 		ld r0,r1	# Load r1 from data memory 
        tst r1		# Test if r1 is 0
-	until gt
+	until nz
 	
 	ldi r0, 0x01	# Load the address for Sy
 	st r0,r1		# Store Sy at 0x01
 	
-	
+	ldi r0, VX
+	ld r0, r0
+	ldi r1, Vx
+	st r1, r0
 	ldi r0, Vx		# Load the address of VxIO
 	do      		# Begin the keyboard read loop
 		ld r0,r1	# Load r1 from data memory 
        tst r1		# Test if r1 is 0
-	until gt
+	until nz
 	
 	ldi r0, 0x02	# Load the address for Vx
 	st r0,r1		# Store Vx at 0x02
 	
-	
+	ldi r0, VY
+	ld r0, r0
+	ldi r1, Vy
+	st r1, r0
 	ldi r0, Vy		# Load the address of VyIO
 	do      		# Begin the keyboard read loop
 		ld r0,r1	# Load r1 from data memory 
        tst r1		# Test if r1 is 0
-	until gt
+	until nz
 
 	# Vy: 0xe0 == 0 => going up		(Vy >= 0)
 	#	   0xe0 == 1 => going down (Vy < 0)
@@ -93,19 +106,19 @@ borris:
 
 	# Begin: Vy = (Vy * Sx)
 	ldi r2, 0x00		# t = Sx
+	ld r2, r2
 	
-	ldi r0, 0x00	# Sx
+	ldi r0, 0x03	# Vy copy
 	ld r0, r0
 	
 	ldi r1, 0x03	# Vy
 	ld r1, r1
 
 	# Vy = (Vy * Sx) 
-	while
-		tst r2		# while t > 0:
-	stays pl
-		add r1, r1	# Vy += Vy
-		neg r2		# t --
+	while			# while t > 0:
+		dec r2		# t --
+	stays nz
+		add r0, r1	# Vy += Vy
 	wend
 	# neg r2
 	# End: Vy = (Vy * Sx)
@@ -148,72 +161,35 @@ borris:
 	# End: Yo = Yo (+/-) t
 	
 	
-	# sign flag for Yo (0 — >0; 1 — <0)
-	if
-		tst r0
-	is	mi
-		neg r0
-		ldi r1, 1
-	else
-		ldi r1, 0
-	fi
-	ld r1, r1
+	# Begin: Ynew %= 32
+	# Ynew loaded into r0
+	ldi r1, 0
+	ldi r2, 32
+	while
+		cmp r0, r1
+	stays mi # negative
+		add r2, r0
+	wend
 	
-	
-	# r0 %= 32
-	shla r0
-	shla r0
-	shla r0
-	shla r0
-	# проверка на "6й бит" (спросить у данила)
-	ldi r2, 0xe1
-	if
-		tst r0
-	is cs
-		ldi r2, 1
-	else
-		ldi r2, 0
-	fi
-	ld r2, r2
-	
-	shr r0
-	shr r0
-	shr r0
-	shr r0
+	ldi r1, 31
+	ldi r2, -32
+	while
+		cmp r1, r0
+	stays mi # while 31 - y stays negative
+		add r2, r0
+	wend
+	# End: Ynew %= 32
 
-
-	if
-		tst r1
-	is z
-		if
-			tst r2
-		is nz
-			# return 32 - (r0 % 32)
-			ldi r3, -32
-			add r3, r0
-			neg r0
-		fi
-	else
-		if
-			tst r2
-		is z
-			# return 32 - (r0 % 32)
-			ldi r3, -32
-			add r3, r0
-			neg r0
-		fi
-	fi
-	
 	ldi r1, 0xf4	# New Y
 	st r1, r0
 	
 	br borris          # Brings execution back to the beggining
 
-#INPUTS>
-#Vx:      dc 1   # replace these with your choice
-#Vy:      dc 1   # of unsigned numbers for testing
-#Sx:      dc 10
-#Sy:      dc 31
+INPUTS>
+VX:      dc 1   # replace these with your choice
+VY:      dc -2   # of unsigned numbers for testing
+SX:      dc 20
+SY:      dc 2
 #ENDINPUTS>
 
 # Y: ds 1    # one byte reserved for the remainder
